e0c42d034c3d52c39026111634111b1e74289e28
Burak Ustun <burakustun54@hotmail.com>
2023-06-22 23:46:56 +0300

    Animation

--- src/pages/Campaign.jsx
+++ src/pages/Campaign.jsx
@@ -6,7 +6,7 @@ const Campaign = () => {
   const campaign = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 
   return (
-    <div className="mt-5">
+    <div className="mt-5 min-h-screen">
       <h1 className="text-center font-bold text-gray-800">TÜM KAMPANYALAR</h1>
       <div className="flex  m-5 h-full ">
         <CampaignFilter />
